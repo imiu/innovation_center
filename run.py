cb5637df7c954812057188408c6a000aa7bea1e2
Alex Frazer <alexadfrazer@gmail.com>
2014-07-04 13:36:46 -0400

    added initdb command

--- run.py
+++ run.py
@@ -1,14 +1,27 @@
 from innovation_center.app import create_app
 from innovation_center.app.extensions import db
+from innovation_center.app.user.models import User
+from innovation_center.app.user.roles import ADMIN
 from flask.ext.script import Manager, Shell
 
 app = create_app('DEVELOPMENT')
 manager = Manager(app)
 
-def make_shell_context():
-    return dict(app=app, db=db)
+@manager.command
+def initdb():
+    db.drop_all()
+    db.create_all()
 
-manager.add_command("shell", Shell(make_context=make_shell_context))
+    admin = User(
+        first_name='Alex',
+        last_name='Frazer',
+        email='dreadsin@gmail.com',
+        role_code=ADMIN,
+        username='admin',
+        password='a-cat-among-men'
+    )
+    db.session.add(admin)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
