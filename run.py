be30d81f1d75ec1d397cf575dae79cb33039ae39
Alex <alexadfrazer@gmail.com>
2014-07-15 13:40:45 -0400

    an experiment

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from .innovation_center.app import create_app
+from innovation_center.app import create_app
 from innovation_center.app.extensions import db
 from innovation_center.app.models import User, NewsArticle
 from innovation_center.app.constants import ADMIN
